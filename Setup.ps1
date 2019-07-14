db2f69e9a93e86752d58b31a32ed8eae53b964c7
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-07-14 23:52:21 +0300

    check distribution

--- Setup.ps1
+++ Setup.ps1
@@ -3,6 +3,26 @@ Install-Module -Name psake
 dotnet tool install --global smite-cli
 dotnet tool update --global smite-cli
 
+$IsFedora = $false
+$IsUbuntu = $false
+
+if ($IsLinux) {
+    $release = Get-Content -Path /etc/os-release
+    $release = $release.Split([Environment]::NewLine) | Where-Object { $_.StartsWith("ID=") }
+    $release = $release.Substring(3)
+
+    $IsFedora = ($release -eq "Fedora")
+    $IsUbuntu = ($release -eq "Ubuntu")
+}
 if ($IsWindows) {
     choco install kubernetes-cli
+}
+elseif ($IsFedora) {
+    Write-Host TBD
+}
+elseif ($IsUbuntu) {
+    Write-Host TBD
+}
+else {
+    Write-Error "OS and/or distribution is not supported"
 }
