bf6dea852ecea7c4f120373161f40da1870923f7
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-07-15 08:00:26 +0300

    Create docker-compose-tasks.ps1

---
+++ docker-compose-tasks.ps1
@@ -0,0 +1,11 @@
+Properties {
+    #
+}
+
+Task Pull {
+    Exec { docker-compose -f (Resolve-Path "docker-compose/dependencies.yml").Path pull }
+}
+
+Task Setup -depends Pull {
+    Exec { docker-compose -f (Resolve-Path "docker-compose/dependencies.yml").Path pull }
+}
