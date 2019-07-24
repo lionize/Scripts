744c4e365bfba9c2be811f84c8e89a91903ebc15
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-07-24 23:24:17 +0300

    Update docker-compose-tasks.ps1

--- docker-compose-tasks.ps1
+++ docker-compose-tasks.ps1
@@ -6,6 +6,17 @@ Task Pull {
     Exec { docker-compose -f (Resolve-Path "docker-compose/dependencies.yml").Path -f (Resolve-Path "docker-compose/microservices.yml").Path pull }
 }
 
-Task Setup -depends Pull {
+Task Run -depends Pull {
     Exec { docker-compose -f (Resolve-Path "docker-compose/dependencies.yml").Path -f (Resolve-Path "docker-compose/microservices.yml").Path up -d }
 }
+
+Task ApplyScript     {
+    $username = "sa"
+    $password = ConvertTo-SecureString -String "z0aiDwJ0205q37Nr" -AsPlainText -Force
+    $credential = New-Object System.Management.Automation.PSCredential($username,$password)
+    Invoke-DbaQuery -SqlInstance "localhost,1433" -SqlCredential $credential -Database "IdentityManagementService" -MessagesToOutput -File "Data.sql" -EnableException
+
+    
+}
+
+Task Setup -depends ApplyScript
